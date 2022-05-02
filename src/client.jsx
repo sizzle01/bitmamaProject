3c6d939b8eafef14c0350c71aa84062f11191ebb
Your Name <you@example.com>
2022-05-02 07:40:09 +0100

    Adding oauth to the app

---
+++ src/client.jsx
@@ -0,0 +1,6 @@
+import {createClient} from '@supabase/supabase-js'
+
+const supabase = createClient('http://localhost:3001/',
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwaW1wdGV3Znh4a3NmeWt5b253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTE0NzEzMjIsImV4cCI6MTk2NzA0NzMyMn0.-48M573eFVbMjnN2M_tklQJw9iawX5tmpLcYK8yeNB4'
+)
+export supabase
