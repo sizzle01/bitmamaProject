105c37cbe02d3bc09ad188bada379418c580cbd0
Your Name <you@example.com>
2022-05-02 11:21:35 +0100

    I added supabse url to client file

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  'https://bitmamatest.netlify.app/',
+  'https://dpimptewfxxksfykyonw.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwaW1wdGV3Znh4a3NmeWt5b253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTE0NzEzMjIsImV4cCI6MTk2NzA0NzMyMn0.-48M573eFVbMjnN2M_tklQJw9iawX5tmpLcYK8yeNB4',
 )
 export { supabase }
