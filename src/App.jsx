3f097f3c34dd9ba5ddecf2dfbfcecaa0c6963e0c
Your Name <you@example.com>
2022-05-02 11:08:24 +0100

    I added Oauth feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import UserPage from './component/userPage/userPage'
+import { supabase } from './client'
 
 function App() {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    /* when the app loads, check to see if the user is signed in */
+    checkUser()
+    /* check user on OAuth redirect */
+    window.addEventListener('hashchange', function () {
+      checkUser()
+    })
+  }, [])
+  async function checkUser() {
+    /* if a user is signed in, update local state */
+    const user = supabase.auth.user()
+    setUser(user)
+  }
+  async function signInWithGithub() {
+    /* authenticate with GitHub */
+    await supabase.auth.signIn({
+      provider: 'github',
+    })
+  }
+  async function signOut() {
+    /* sign the user out */
+    await supabase.auth.signOut()
+    setUser(null)
+  }
+
+  if (user) {
+    return (
+      <div className="App">
+        <h1>Hello, {user.email}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </div>
+    )
+  }
+  return (
+    <div className="App">
+      <h1>Hello, please sign in!</h1>
+      <button onClick={signInWithGithub}>Sign In</button>
+    </div>
+  )
   return (
     <div className="App">
       <>
--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,7 @@
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('http://localhost:3001/',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwaW1wdGV3Znh4a3NmeWt5b253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTE0NzEzMjIsImV4cCI6MTk2NzA0NzMyMn0.-48M573eFVbMjnN2M_tklQJw9iawX5tmpLcYK8yeNB4'
+const supabase = createClient(
+  'https://bitmamatest.netlify.app/',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwaW1wdGV3Znh4a3NmeWt5b253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTE0NzEzMjIsImV4cCI6MTk2NzA0NzMyMn0.-48M573eFVbMjnN2M_tklQJw9iawX5tmpLcYK8yeNB4',
 )
-export supabase
+export { supabase }
